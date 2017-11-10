620abf672cfc0ecb34a84ee153aba7c623d46772
Damir Rinatovich <Damir>
2017-11-10 13:17:02 +0300

    Добавлена новая функция в API

--- core/sync/API/client.php
+++ core/sync/API/client.php
@@ -8,7 +8,7 @@ function save_source_booking_data($connect, $data) {
   $telephone = $data["telephone"];
   $email = trim($data["email"]);
   $today = date("Y-m-d");
-  if(count($telephone) > 0)
+  if(mb_strlen($telephone) > 0)
   	$id = $connect->getOne("SELECT id FROM klient WHERE login=?s OR email=?s OR telephone=?s LIMIT 1", $email, $email, $telephone);
   else
     $id = $connect->getOne("SELECT id FROM klient WHERE login=?s OR email=?s LIMIT 1", $email, $email);
