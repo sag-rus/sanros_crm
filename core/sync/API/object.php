030be68db8c0ac6800af3a4d8fc620ca23dee89a
Damir Rinatovich <Damir>
2017-12-28 18:49:50 +0300

    Обновление проекта

--- core/sync/API/object.php
+++ core/sync/API/object.php
@@ -588,7 +588,8 @@ function display_invoice_payment_comparison_price($connect, $data){
       'contract_request_info' => json_encode($info)
     ]);
 
-    $edit->addPaymentInvoice($info['rate'], $info['month'], $info['date']);
+    $module_id = $this->connect->getOne("SELECT id FROM comparison_module_object WHERE object=?i", $data["object"]);
+    $this->connect->query("INSERT INTO comparison_module_payment_invoice(module_id, rate, month, date, status) VALUES(?i, ?i, ?i, ?i, ?i)", $module_id, $info['rate'], $info['month'], $info['date'], 1);
 		return $array;
 	}
 }
