0906d09b93bf5655497892c95b83d1e6c483c7d5
Damir Rinatovich <Damir>
2018-01-30 16:09:34 +0300

    Настройки доступа

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "10-06-2017";
+	$version = "11-06-2017";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
         'userName' => 'sanata_trevel-api',
