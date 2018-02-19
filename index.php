83a5288d8d93c6ea59472437c665320a54aed25c
Damir Rinatovich <Damir>
2018-02-19 12:34:21 +0300

    Update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "20-06-2017";
+	$version = "21-06-2017";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
