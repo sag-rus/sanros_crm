b67b41791792522d8a1aace788d61d1142b8fb4b
Damir Rinatovich <Damir>
2018-02-20 20:35:51 +0300

    Обновление API по холдированию

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "21-06-2017";
+	$version = "22-06-2017";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
