1ebf4bdfe1c331f35d2a26966c931ff7da462831
Дамир Хаев < >
2018-05-18 18:39:43 +0300

    Обновление API

--- app/lib/payment/Sberbank/BookingPayment.php
+++ app/lib/payment/Sberbank/BookingPayment.php
@@ -311,6 +311,7 @@ class BookingPayment extends Client {
       $answer["sum"] = add_null($sum["to-pay"]);
       $answer["bonus"] = add_null($sum["bonus"]);
       $answer["prepay"] = add_null($sum["prepay"]);
+      $answer['fast_booking'] = $connect->getOne("SELECT fast_booking FROM object WHERE id = ?i",$array["id_obj"]);
       $turist = new Display($client);
       $answer["turist"] = $turist->selectFio();
       unset($turist);
