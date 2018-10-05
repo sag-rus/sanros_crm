662330dfee93653d66ed77ec0038bcec3cbc533b
Дамир Хаев < >
2018-10-05 03:20:29 +0300

    Обновление API

--- core/admin/news.php
+++ core/admin/news.php
@@ -540,7 +540,7 @@ function set_sites_content($connect) {
 }
 
 function sync_site_content($connect, $id):bool {
-    $content = $connect->getRow("SELECT `id` AS `source_id`, `status`, `published`, `type`, `site_id`, `title`, `summary`, `body`, `path`, `description`, `keywords`, `image`, `weight` FROM `sites_contents` WHERE `id` =?i",$id);
+    $content = $connect->getRow("SELECT `id` AS `source_id`, `status`, `published`, `type`, `site_id`, `title`, `summary`, `body`, `path`, `description`, `keywords`, `image`, `weight`, `module_object_id`, `module_block` FROM `sites_contents` WHERE `id` =?i",$id);
     if($content) {
         try {
           $client = new \GuzzleHttp\Client();
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "08-08-2017";
+	$version = "09-08-2017";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
