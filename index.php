976609053a1fc1c6a936cc5991a7e6d9ade0e66d
Дамир Хаев < >
2018-10-25 17:13:52 +0300

    Выгрузка адресов

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "20-08-2017";
+	$version = "21-08-2017";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
