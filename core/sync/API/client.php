25dd5e6026d1fb4ad094c593ce6eb34acac81570
Дамир Хаев < >
2018-12-05 01:17:57 +0300

    Bug

--- core/sync/API/client.php
+++ core/sync/API/client.php
@@ -439,9 +439,9 @@ function show_tour_bid_account($connect, $data){
 		}
 		elseif ($array['fast_booking']) {
       $reward = get_reward_schet($connect, $array['id']);
-      if (!((($reward / $answer["sum"] * 100) >= 4 || $array['id'] == 43125 || $array['id'] == 58509 || $array['id'] = 66300) && !$array['holding_sum'])) {
+      //if (!((($reward / $answer["sum"] * 100) >= 4 || $array['id'] == 43125 || $array['id'] == 58509 || $array['id'] = 66300) && !$array['holding_sum'])) {
       	$array['fast_booking'] = 0;
-      }
+      //}
     }
 
 		if($answer["status"] == 5)
