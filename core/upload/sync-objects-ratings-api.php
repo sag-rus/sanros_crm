4f6eb9c93148d3ebf4c887ba5dc2af649ab4bb67
Damir Haev <damirhaev@gmail.com>
2019-04-11 18:13:06 +0300

    Обновление

--- core/upload/sync-objects-ratings-api.php
+++ core/upload/sync-objects-ratings-api.php
@@ -12,15 +12,15 @@ function sync_objects_ratings_api($connect){
 			$ratingAr = [
 				'id' => $rating['id'],
 				'token' => '7db0d2680968f87e33dd3db9a4b5db38d373ba8a9f42ca7dc97d6f14711efaa4',
-				'status' => $rating['status'] > 2?1:0,
+				'status' => ($rating['status'] > 2?1:0),
 				'created' => strtotime($rating['date_send']),
 				'resort_id' => $rating['id_obj'],
-				'positive' => $rating['positive'],
-				'negative' => $rating['negative'],
-				'advice' => $rating['advice'],
-				'author_name' => $rating['turist'],
+				'positive' => (string)$rating['positive'],
+				'negative' => (string)$rating['negative'],
+				'advice' => (string)$rating['advice'],
+				'author_name' => (string)$rating['turist'],
 				'average' => round(($rating['clean']+$rating['comfort']+$rating['location']+$rating['staff']+$rating['ratio']+$rating['leisure']+$rating['treatment'])/14,1),
-				'company_rating' => $rating['company_rating'],
+				'company_rating' => (string)$rating['company_rating'],
 				'uid' => 1
 			];
 
