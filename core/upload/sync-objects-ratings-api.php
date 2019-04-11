76affaa9b7d774f7204360c42444e290bcd8d51b
Damir Haev <damirhaev@gmail.com>
2019-04-11 18:09:41 +0300

    Обновление

--- core/upload/sync-objects-ratings-api.php
+++ core/upload/sync-objects-ratings-api.php
@@ -10,6 +10,7 @@ function sync_objects_ratings_api($connect){
 		$ratings = $connect->getAll("SELECT `id`, `status`, `clean`, `comfort`, `location`, `staff`, `ratio`, `leisure`, `treatment`, `id_obj`, `positive`, `negative`, `date_send`, `company_rating`, `turist`, `advice` FROM `rating` WHERE `date_send` IS NOT NULL AND `average` > 0 AND `synchronized` = 0 AND `id_obj` > 0");
 		foreach ($ratings as $rating) {
 			$ratingAr = [
+				'id' => $rating['id'],
 				'token' => '7db0d2680968f87e33dd3db9a4b5db38d373ba8a9f42ca7dc97d6f14711efaa4',
 				'status' => $rating['status'] > 2?1:0,
 				'created' => strtotime($rating['date_send']),
@@ -28,15 +29,11 @@ function sync_objects_ratings_api($connect){
 			]);
 
 			$res = json_decode($res->getBody(),true);
-			print_r($res);
 
 			if(array_key_exists('success',$res)) {
 				$success = (bool)(int)$res['success'];
 				if($success) {
 						$connect->query("UPDATE `rating` SET `synchronized` = '1' WHERE `id` = ?i", $rating['id']);
-				}
-				else {
-
 				}
 			}
 			break;
