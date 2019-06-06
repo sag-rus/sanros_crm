e900be2d4b8c3a8889f70e5893bce93274887ba9
Damir Haev <damirhaev@gmail.com>
2019-06-06 17:07:35 +0300

    Обновление

--- core/sync/fast-requests-0.php
+++ core/sync/fast-requests-0.php
@@ -184,7 +184,7 @@ if(is_null($last_time) || file_exists($directory."/core/sync/file/fast-killing-t
             }
 
             try {
-              $res = $client->request('POST',"https://sync.tonia.ru/api/request/answer/set/".$id,[
+              $res = $client->request('POST',"https://sync.tonia.ru/api/request/answer/set/".$id.'?cache='.substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'), 1, 15),[
                 'form_params' => [
                   'token' => $token,
                   'answer' => $respAr
