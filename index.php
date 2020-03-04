c10ca740ab26bd443acc103241ab425597f53349
Damir Haev <damirhaev@gmail.com>
2020-03-05 02:54:07 +0300

    Update

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
-    ]); */
-    echo urldecode(explode('/', 'https://xn----7sba6aaba8akdsdekah.xn--p1ai/%D0%BD%D0%B0%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F/%D1%81%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D1%8B%D0%B9-%D0%BA%D0%B0%D0%B2%D0%BA%D0%B0%D0%B7',4)[3]);
-    die();
+    ]);
+    die();*/
 
 ?>
 
