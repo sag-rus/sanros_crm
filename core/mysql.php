ed3af770a809cbb96416f264361c2f2f3383ffdc
Vladimir Dulov <rus.custom@gmail.com>
2021-03-31 16:52:36 +0400

    пересмотрены права доступа для всех уровней пользователей (rights: 1 - 6)

--- core/mysql.php
+++ core/mysql.php
@@ -672,7 +672,7 @@ function write_body($connect){
 ?>
 	<div class="wrapper">
 		<div class="main-header">
-            <?php if($id_rights > 1) { ?>
+            <?php if($id_rights > 3) { ?>
 			<span>
 				<input type="text" class="form-control input-sm number-reckoning" style="width: 150px; margin: 7px; display: inline" placeholder="Поиск по заявке" onKeyPress="if(event.keyCode == 13) select_by_number_reckoning()" />
 			</span>
@@ -716,36 +716,37 @@ function write_body($connect){
 				</div>
 
 				<ul class="nav nav-pills nav-stacked head-menu">
-                  <?php if($id_rights > 1) { ?>
+                  <?php if($id_rights > 3) { ?>
 					<li onclick="head_page()" id="reckoning-menu"><a>Заявки</a></li>
 					<li onclick="show_call_back_menu()" id="call-back-menu"><a>Заказы звонка</a></li>
                     <li id="new_klient_menu" onclick="add_klient()"><a>Клиент</a></li>
 					<li id="agency_menu" onclick="agency()"><a>Агентства</a></li>
 					<li id="touroperator_menu" onclick="touroperator()"><a>Туроператоры</a></li>
 				  <?php } ?>
-					<li id="obj_menu" onclick="objects(<?=$id_rights;?>)" data-id-rights="<?=$id_rights;?>"><a>Объекты</a></li>
 				  <?php if($id_rights > 1) { ?>
+					<li id="obj_menu" onclick="objects(<?=$id_rights;?>)" data-id-rights="<?=$id_rights;?>"><a>Объекты</a></li>
+				  <?php } ?>
+				  <?php if($id_rights > 3) { ?>
 					<li id="reminder_menu" onclick="my_reminder()"><a>Напоминания</a></li>
                   <?php } ?>
-			<?php if($id_rights == 1 || $id_rights > 2 || $session_login == 21 || $session_login == 55){ ?>
+				  <?php if($id_rights > 3 || $session_login == 21 || $session_login == 55){ ?>
 					<li id="report_menu" onclick="show_reports()" data-id-rights="<?=$id_rights;?>"><a>Отчеты</a></li>
-                    <?php if($id_rights > 1) { ?>
-                    <li id="filter_menu" onclick="show_filter()"><a>Поиск</a></li>
-                    <?php } ?>
-			<?php }elseif($id_rights > 1) { ?>
+				  <?php } ?>
+				  <?php if($id_rights > 3) { ?>
 					<li id="filter_menu" onclick="show_filter()"><a>Поиск</a></li>
-			<?php } ?>
-			<?php if($id_rights > 3){ ?>
+				  <?php } ?>
+				  <?php if($id_rights > 4){ ?>
 					<li class="manager-menu"><a onclick="$('.menu-manager').toggle()">Менеджеры</a>
 						<ul class="nav nav-pills nav-stacked second-level-menu menu-manager" style="display: none">
 							<li onclick="see_managers()" id="plan-manager"><a><i class="fa fa-rub"></i> План</a></li>
 							<li onclick="show_chat_users()" id="chat-manager"><a><i class="fa fa-weixin"></i> Чат</a></li>
 						</ul>
 					</li>
-			<?php } ?>
-                  <?php if($id_rights > 1) { ?>
+				  <?php } ?>
+                  <?php if($id_rights > 3) { ?>
 					<li onclick="show_certificate()" id="certificate_menu"><a>Сертификаты</a></li>
                   <?php } ?>
+                  <?php if($id_rights > 3) { ?>
 					<li class="question-menu"><a onclick="$('.menu-question').toggle()">Вопросы</a>
 						<ul class="nav nav-pills nav-stacked second-level-menu menu-question" style="display: none">
 							<li onclick="show_question_client()" id="question-turist" class="question-turist"><a><i class="fa fa-circle-o"></i> Турист</a></li>
@@ -753,16 +754,17 @@ function write_body($connect){
 							<li onclick="show_question_object()" id="question-object" class="question-object"><a><i class="fa fa-circle-o"></i> Объект</a></li>
 						</ul>
 					</li>
+                  <?php } ?>
 					<li><a onclick="$('.menu-profile').toggle()">Профиль</a>
 						<ul class="nav nav-pills nav-stacked second-level-menu menu-profile" style="display: none">
-							<?php if($id_rights > 1 && $id_rights <= 3){ ?>
+							<?php if($id_rights > 3){ ?>
 							<li onclick="show_profit()" id="commission_menu"><a><i class="fa fa-rub"></i> Мой доход</a></li>
 							<?php } ?>
 							<li onclick="show_change_password()" id="my-password"><a><i class="fa fa-key"></i> Сменить пароль</a></li>
 							<li onclick="show_my_chat_log()" id="my-chat-log"><a><i class="fa fa-weixin"></i> Чат</a></li>
 						</ul>
 					</li>
-			<?php if($id_rights > 0){ ?>
+				  <?php if($id_rights > 1){ ?>
 					<li id="all-admin-menu"><a onclick="$('.menu-admin').toggle()">Админ</a>
 						<ul class="nav nav-pills nav-stacked second-level-menu menu-admin" style="display: none">
 							<li onclick="object()" id="price_menu"><a><i class="fa fa-home"></i> Объекты</a></li>
@@ -790,7 +792,8 @@ function write_body($connect){
                           <?php } ?>
 						</ul>
 					</li>
-
+				  <?php } ?>
+				  <?php if($id_rights > 4){ ?>
 					<li class="menu-object-cabinet"><a onclick="$('.show-menu-object-cabinet').toggle()">Кабинет объекта</a>
 						<ul class="nav nav-pills nav-stacked second-level-menu show-menu-object-cabinet" style="display: none">
 							<li onclick="show_cabinet_object()" id="account-object"><a><i class="fa fa-flag-o"></i> Аккаунты</a></li>
@@ -800,11 +803,9 @@ function write_body($connect){
 							<li onclick="select_object_profkurort()" id="profkurort-menu"><a><i class="fa fa-product-hunt"></i> Профкурорт</a></li>
 						</ul>
 					</li>
-                <?php } ?>
-
-                    <?php if($id_rights > 4 || $session_login == 62) { ?>
-
-                      <li><a onclick="$('.show-menu-sites').toggle()">Сайты</a>
+                  <?php } ?>
+                  <?php if($id_rights > 2 || $session_login == 62) { ?>
+                    <li><a onclick="$('.show-menu-sites').toggle()">Сайты</a>
                         <ul class="nav nav-pills nav-stacked second-level-menu show-menu-sites" style="display: none">
                             <li onclick="show_sites_list()" id="sites-list"><a><i class="fa fa-list"></i> Список</a></li>
                         </ul>
