11fe2429d049eb35abfa1159296b210a190f123f
darthvader <damirhaev@gmail.com>
2021-07-14 06:14:09 +0300

    Update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "10-06-2021";
+	$version = "11-06-2021";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
