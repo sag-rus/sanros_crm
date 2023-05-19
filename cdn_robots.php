38126943b92929350c6882f1d3487d8c5446a9c6
rustem.sagdiev <rustem.sagdiev@corp.mail.ru>
2023-05-19 14:04:36 +0300

    fix

--- cdn_robots.php
+++ cdn_robots.php
@@ -32,35 +32,43 @@ $connect = connect_to_MySQL_directory();
 if(!$connect)
 	return;
 
-/*$client = new GuzzleHttp\Client(['verify' => false]);
-$token = "7db0d2680968f87e33dd3db9a4b5db38d373ba8a9f42ca7dc97d6f14711efaa4";*/
+$client = new GuzzleHttp\Client(['verify' => false]);
 
 $robots = 'User-agent: *'.PHP_EOL;
 
 function AddToRobots($images) {
 	global $robots;
 	foreach ($images as $image) {
-		$robots .= 'Disallow: /*'.substr($image['uri'], strrpos($image['uri'], '/'), 250).PHP_EOL;
+		$filename = $image['uri'];
+		$filename = substr($filename, strrpos($filename, '/'), 250);
+		$filename = substr($filename, 0, strrpos($filename, '.'));
+		$robots .= 'Disallow: *'.$filename.'.*'.PHP_EOL;
 	}
 }
 
 $content = $connect->getAll("SELECT `id` FROM `sites_contents` WHERE `imgs_no_index` =1 ");
 foreach ($content as $item) {
-    $entity = [
-      'id' => $item['id'],
-      'type' => 'content'
-    ];
-    $images = bounds_to_files($connect,load_bounds($connect,$entity,'image'));
-    AddToRobots($images);
-    $images = bounds_to_files($connect,load_bounds($connect,$entity,'photogallery'));
-    AddToRobots($images);
-    $images = bounds_to_files($connect,load_bounds($connect,$entity,'slider_photos'));
+  $entity = [
+    'id' => $item['id'],
+    'type' => 'content'
+  ];
+  $images = bounds_to_files($connect,load_bounds($connect,$entity,'image'));
+  AddToRobots($images);
+  $images = bounds_to_files($connect,load_bounds($connect,$entity,'photogallery'));
+  AddToRobots($images);
+  $images = bounds_to_files($connect,load_bounds($connect,$entity,'slider_photos'));
 	AddToRobots($images);
-    $images = bounds_to_files($connect,load_bounds($connect,$entity,'slider_photos_mobile'));
+  $images = bounds_to_files($connect,load_bounds($connect,$entity,'slider_photos_mobile'));
 	AddToRobots($images);
-    $images = bounds_to_files($connect,load_bounds($connect,$entity,'page_bg'));
+  $images = bounds_to_files($connect,load_bounds($connect,$entity,'page_bg'));
 	AddToRobots($images);	
 }
+
+$res = $client->request('POST', "https://cdn.tonia.ru/api/files/upload/robots" . '?cache=' . substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'), 1, 15), [
+	'form_params' => [
+		'file' => base64_encode($robots)
+	]
+]);
 
 echo $robots;
 
