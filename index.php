9b5a35f99ad18c24b451702b853c750deeabed97
rustem.sagdiev <rustem.sagdiev@corp.mail.ru>
2023-07-11 18:04:39 +0300

    video insert updates

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "07-06-2023";
+	$version = "11-07-2023";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
