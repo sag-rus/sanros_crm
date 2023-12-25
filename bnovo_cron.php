814ad5335c8bec14ed82e96feec5cfb14ae6c2e4
Rustem S <rustem.s@wishlistgames.com>
2023-12-25 22:27:55 +0300

    bnovo_cron

--- bnovo_cron.php
+++ bnovo_cron.php
@@ -73,15 +73,20 @@ echo 'token='.$result['token'];*/
 
 //AUTH
 
-function get_bnovo_rooms_availability($id_obj, $account_id, $dfrom, $dto, $id_rooms_bnovo = false) {
+function get_bnovo_rooms_availability($id_obj, $account_id, $dfrom, $dto, $id_room_bnovo = false) {
 	global $connect;
 
 	$token = get_bnovo_token($connect);
 
-	$all_rooms = $connect->getAll("SELECT * FROM `bnovo_rooms_mathes` WHERE `id_obj`=?i", $id_obj);
-	$rooms = [];
-	foreach ($all_rooms as $room) {
-		$rooms[] = $room['id_bnovo'];
+	if (!$id_room_bnovo) {
+		$all_rooms = $connect->getAll("SELECT * FROM `bnovo_rooms_mathes` WHERE `id_obj`=?i", $id_obj);
+		$rooms = [];
+		foreach ($all_rooms as $room) {
+			$rooms[] = $room['id_bnovo'];
+		}
+	} else {
+		$rooms = [];
+		$rooms[] = $id_room_bnovo;
 	}
 	
 	if (count($rooms)>0) {
@@ -94,7 +99,9 @@ function get_bnovo_rooms_availability($id_obj, $account_id, $dfrom, $dto, $id_ro
 			'for_ota' => 1
 		);
 
+		echo '<pre>$data=';
 		print_r($data);
+		echo '</pre>';
 
 		$ch = curl_init('https://api.reservationsteps.ru/v1/api/availability?' . http_build_query($data));
 
@@ -128,8 +135,10 @@ function get_bnovo_rooms_availability($id_obj, $account_id, $dfrom, $dto, $id_ro
 
 //get_bnovo_rooms_availability($id_obj, $account_id, date('Y-m-d'), date('Y-m-d', time()+86400*30));
 
-function get_bnovo_rooms_prices($id_obj, $account_id, $dfrom, $dto, $id_rooms_bnovo = false) {
+function get_bnovo_rooms_prices($id_obj, $account_id, $dfrom, $dto, $id_plan_bnovo = false, $id_room_bnovo = false) {
 	global $connect;
+
+	echo 'get_bnovo_rooms_prices: id_obj='.$id_obj.' $account_id='.$account_id.' $dfrom='.$dfrom.' $dto='.$dto.' plan='.$id_plan_bnovo.' room='.$id_room_bnovo;
 
 	$token = get_bnovo_token($connect);
 
@@ -139,14 +148,20 @@ function get_bnovo_rooms_prices($id_obj, $account_id, $dfrom, $dto, $id_rooms_bn
 		$qplans[] = $plan['id_bnovo'];
 		$plans[$plan['id_bnovo']] = $plan['id_plan'];
 	}
-	echo 'plans';
-	print_r($plans);
-
+	if ($id_plan_bnovo) {
+		$qplans = [];
+		$qplans[] = $id_plan_bnovo;
+	}
+	
 	$all_rooms = $connect->getAll("SELECT * FROM `bnovo_rooms_mathes` WHERE `id_obj`=?i", $id_obj);
 	$qrooms = $rooms = [];
 	foreach ($all_rooms as $room) {
 		$qrooms[] = $room['id_bnovo'];
 		$rooms[$plan['id_bnovo']] = $room['id_room'];
+	}
+	if ($id_room_bnovo) {
+		$qrooms = [];
+		$qrooms[] = $id_room_bnovo;
 	}
 
 	$all_occus = $connect->getAll("SELECT * FROM `bnovo_occupancies_mathes` WHERE `id_obj`=?i", $id_obj);
@@ -155,11 +170,11 @@ function get_bnovo_rooms_prices($id_obj, $account_id, $dfrom, $dto, $id_rooms_bn
 		$temp = [];
 		$temp['id_place'] = $occu['id_place'];
 		$temp['id_room'] = $occu['id_room'];
-		$occus[$occu['id_bnovo']] = $temp;
+		$occus[$occu['id_bnovo']][] = $temp;
 	}
 
 
-	if (count($plans)>0 && count($rooms)>0) {
+	if (count($qplans)>0 && count($qrooms)>0) {
 		$data = array(
 			'token'  => $token,
 			'account_id' => $account_id,
@@ -170,7 +185,9 @@ function get_bnovo_rooms_prices($id_obj, $account_id, $dfrom, $dto, $id_rooms_bn
 			'fields' => ['price','min_stay','min_stay_arrival']
 		);
 
+		echo '<pre>$data=';
 		print_r($data);
+		echo '</pre>';
 
 		$ch = curl_init('https://api.reservationsteps.ru/v1/api/plans_data?' . http_build_query($data));
 
@@ -199,25 +216,37 @@ function get_bnovo_rooms_prices($id_obj, $account_id, $dfrom, $dto, $id_rooms_bn
 					}
 					echo 'id_date_price='.$id_date_price."\r\n";
 
-					//Проверяем наличие нужных записей в ranges
-					if($connect->getOne("SELECT COUNT(*) FROM ranges WHERE `id_obj`=$id_obj AND `type`=1 AND `place`='".$occus[$id_room_bnovo]['id_place']."' AND `id_date`=$id_date_price AND `rate_plan`='".$plans[$id_plan_bnovo]."'") == 0) {
-						echo "SELECT COUNT(*) FROM ranges WHERE `id_obj`=$id_obj AND `type`=1 AND `place`='".$occus[$id_room_bnovo]['id_place']."' AND `id_date`=$id_date_price AND `rate_plan`='".$plans[$id_plan_bnovo]."'"."\r\n";
-						$connect->query("INSERT INTO `ranges` SET `id`=0, `name`='from_bnovo', `id_obj`=$id_obj, `active`=0, `show_date`=1, `place`='".$occus[$id_room_bnovo]['id_place']."', `id_date`=$id_date_price, `counter`=1, `rate_plan`=".$plans[$id_plan_bnovo].", `treatment`=0");
-						$id_range = $connect->insertId();
-						echo "INSERT INTO `ranges` SET `id`=0, `name`='from_bnovo', `id_obj`=$id_obj, `active`=0, `show_date`=1, `place`='".$occus[$id_room_bnovo]['id_place']."', `id_date`=$id_date_price, `counter`=1, `rate_plan`=".$plans[$id_plan_bnovo].", `treatment`=0"."\r\n";
-					} else {
-					    $id_range = $connect->getOne("SELECT id FROM ranges WHERE `id_obj`=$id_obj AND `type`=1 AND `place`='".$occus[$id_room_bnovo]['id_place']."' AND `id_date`=$id_date_price AND `rate_plan`='".$plans[$id_plan_bnovo]."'");
-					}
-					echo 'id_range='.$id_range."\r\n";
+					if (count($occus[$id_room_bnovo])>0) {
+						echo '<pre>$occus[$id_room_bnovo]';
+						print_r($occus[$id_room_bnovo]);
+						echo '</pre>';
+						foreach($occus[$id_room_bnovo] as $occu) {
+							//Проверяем наличие нужных записей в ranges
+							if($connect->getOne("SELECT COUNT(*) FROM ranges WHERE `id_obj`=$id_obj AND `type`=1 AND `place`='".$occu['id_place']."' AND `id_date`=$id_date_price AND `rate_plan`='".$plans[$id_plan_bnovo]."'") == 0) {
+								echo "SELECT COUNT(*) FROM ranges WHERE `id_obj`=$id_obj AND `type`=1 AND `place`='".$occu['id_place']."' AND `id_date`=$id_date_price AND `rate_plan`='".$plans[$id_plan_bnovo]."'"."\r\n";
+								$connect->query("INSERT INTO `ranges` SET `id`=0, `name`='from_bnovo', `id_obj`=$id_obj, `active`=0, `show_date`=1, `place`='".$occu['id_place']."', `id_date`=$id_date_price, `counter`=1, `rate_plan`=".$plans[$id_plan_bnovo].", `treatment`=0");
+								$id_range = $connect->insertId();
+							} else {
+								$id_range = $connect->getOne("SELECT id FROM ranges WHERE `id_obj`=$id_obj AND `type`=1 AND `place`='".$occu['id_place']."' AND `id_date`=$id_date_price AND `rate_plan`='".$plans[$id_plan_bnovo]."'");
+							}
+							echo 'id_range='.$id_range."\r\n";
 
-					if($connect->getOne("SELECT COUNT(*) FROM price WHERE `id_room`=".$occus[$id_room_bnovo]['id_room']." AND `id_range`=$id_range") == 0) {
-						echo "SELECT COUNT(*) FROM price WHERE `id_room`=".$occus[$id_room_bnovo]['id_room']." AND `id_range`=$id_range"."\r\n";
-						$connect->query("INSERT INTO `price` SET `id`=0, `id_room`='".$occus[$id_room_bnovo]['id_room']."', `price`=".$data['price'].", `id_range`=$id_range, `active`=0, `date_last_save`='".date('H:i:s d.m.Y')."', `manager`='from bnovo'");
-						$id_range = $connect->insertId();
-						echo "INSERT INTO `price` SET `id`=0, `id_room`='".$occus[$id_room_bnovo]['id_room']."', `price`=".$data['price'].", `id_range`=$id_range, `active`=0, `date_last_save`='".date('H:i:s d.m.Y')."', `manager`='from bnovo'"."\r\n";
-					} else {
-					    $connect->query("UPDATE price SET `price`=?i WHERE `id_room`=".$occus[$id_room_bnovo]['id_room']." AND `id_range`=$id_range", $data['price']);
-					}					
+							echo '<pre>';
+							print_r($occu);
+							echo '</pre>';
+							if ($id_range>0) {							
+								if($connect->getOne("SELECT COUNT(*) FROM price WHERE `id_room`=".$occu['id_room']." AND `id_range`=$id_range") == 0) {
+									echo "SELECT COUNT(*) FROM price WHERE `id_room`=".$occu['id_room']." AND `id_range`=$id_range"."\r\n";
+									$connect->query("INSERT INTO `price` SET `id`=0, `id_room`='".$occu['id_room']."', `price`=".$data['price'].", `id_range`=$id_range, `active`=0, `date_last_save`='".date('H:i:s d.m.Y')."', `manager`='from bnovo'");
+									$id_range = $connect->insertId();
+									echo "INSERT INTO `price` SET `id`=0, `id_room`='".$occu['id_room']."', `price`=".$data['price'].", `id_range`=$id_range, `active`=0, `date_last_save`='".date('H:i:s d.m.Y')."', `manager`='from bnovo'"."\r\n";
+								} else {
+									$connect->query("UPDATE price SET `price`=?i, `synchronized`=0 WHERE `id_room`=".$occu['id_room']." AND `id_range`=$id_range", $data['price']);
+									echo $connect->last_query()."\r\n";
+								}
+							}
+						}
+					}
 
 					
 				}
@@ -228,8 +257,55 @@ function get_bnovo_rooms_prices($id_obj, $account_id, $dfrom, $dto, $id_rooms_bn
 
 }
 
-get_bnovo_rooms_prices($id_obj, $account_id, date('Y-m-d'), date('Y-m-d', time()+86400*14));
+//get_bnovo_rooms_prices($id_obj, $account_id, date('Y-m-d'), date('Y-m-d', time()+86400*14));
 
-echo '4444';
+//echo '4444';
+
+$data = $connect->getRow("SELECT * FROM `bnovo_data_updates` WHERE `worked`=0 ORDER BY id ASC LIMIT 1 ");
+if ($data && $data['data']!='') {
+	//$connect -> query("UPDATE `bnovo_data_updates` SET `worked`=1, `worked_datetime`=NOW() WHERE `id`=$data[id]");
+	echo $connect->last_query()."\r\n";
+	$data = json_decode($data['data'], true);
+
+	$data['hotel_id'] = $id_obj;
+
+	if (count($data['data']['prices'])>0) {
+		ECHO '1111';
+		$prices = $data['data']['prices'];
+		foreach ($prices as $id_plan_bnovo => $rooms_data) {
+			foreach ($rooms_data as $id_room_bnovo => $dates) {
+				$mindate = 99999999999; $maxdate = 0;
+				foreach ($dates as $date) {
+					if (strtotime($date)<$mindate) $mindate = strtotime($date);
+					if (strtotime($date)>$maxdate) $maxdate = strtotime($date);
+				}
+
+				if ($maxdate<>0 && $mindate<>99999999999) {
+					echo 'mindate='.date('Y-m-d', $mindate).' maxdate='.date('Y-m-d', $maxdate).' id_room_bnovo='.$id_room_bnovo.' id_plan_bnovo='.$id_plan_bnovo."\r\n";
+					get_bnovo_rooms_prices($data['hotel_id'], $data['account_id'], date('Y-m-d', $mindate), date('Y-m-d', $maxdate), $id_plan_bnovo, $id_room_bnovo);
+				}
+			}
+		}
+	}
+
+	if (count($data['data']['rooms'])>0) {
+		$rooms = $data['data']['rooms'];
+		echo '<pre>';
+		print_r($rooms);
+		echo '</pre>';
+		foreach ($rooms as $id_room_bnovo => $dates) {
+			$mindate = 99999999999; $maxdate = 0;
+			foreach ($dates as $date) {
+				if (strtotime($date)<$mindate) $mindate = strtotime($date);
+				if (strtotime($date)>$maxdate) $maxdate = strtotime($date);
+			}
+
+			if ($maxdate<>0 && $mindate<>99999999999) {
+				echo 'mindate='.date('Y-m-d', $mindate).' maxdate='.date('Y-m-d', $maxdate).' id_room_bnovo='.$id_room_bnovo."\r\n";
+				get_bnovo_rooms_availability($data['hotel_id'], $data['account_id'], date('Y-m-d', $mindate), date('Y-m-d', $maxdate), $id_room_bnovo);
+			}
+		}
+	}
+}
 
 ?>
