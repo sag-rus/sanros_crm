19fe6dffa929c12ab7cd3d64519d7c67db6defe4
Rustem S <rustem.s@wishlistgames.com>
2024-01-22 18:25:26 +0300

    bnovo-cancel

--- bnovo-cancel.php
+++ bnovo-cancel.php
@@ -54,6 +54,29 @@ $configNew->objectCabinet = $objectCabinet;
 $row = $connect->getRow("SELECT * FROM reckoning WHERE id=?i", $_GET['id']);
 if ($row['id']==$_GET['id']) {
     $connect->query("UPDATE `reckoning` SET `bnovo`=2, `status`=6 WHERE `id`=?i", $row['id']);
+
+    $data = json_decode($row['bnovo_json'], true);
+    $data['booking_data']['status_id']=2;
+    $data['token'] = get_bnovo_token($connect);
+    $data = json_encode($data);
+
+	$ch = curl_init('https://api.reservationsteps.ru/v1/api/channel_manager_bookings'); 
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$res = json_decode(curl_exec($ch), true);
+	curl_close($ch);    
+
+    /*echo '<pre>';
+    print_r($data);
+    echo '</pre>';*/
+
+    echo '<pre>';
+    print_r($res);
+    echo '</pre>';
 }
 
 ?>
