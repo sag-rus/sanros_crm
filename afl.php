a355759d699b6466ff17cd492a5b6c96d33c9317
Rustem S <rustem.s@wishlistgames.com>
2024-04-01 19:00:58 +0300

    dbg

--- afl.php
+++ afl.php
@@ -94,6 +94,7 @@ if ($_GET['send']=='1') {
     $email = 'esuschenko@aeroflot.ru';
 
     send_mail($email, 'Отчет по начисленным милям', 'Отчет по начисленным милям', false, false, '/var/www/html/CRM/afl.txt');
+    send_mail('sagrus@yandex.ru', 'Отчет по начисленным милям', 'Отчет по начисленным милям', false, false, '/var/www/html/CRM/afl.txt');
 
     echo '<br><br><strong>Письмо с файлом отправлено на '.$email.'</strong>';
 
