ee7bff98abcc115ab98e06a1a20811c48d246e13
Rustem S <rustem.s@wishlistgames.com>
2024-05-13 19:53:33 +0300

    dbg

--- core/sync/vpn-requests-cabinet.php
+++ core/sync/vpn-requests-cabinet.php
@@ -121,7 +121,7 @@
     if(!$connect){
         exit();
     }
-    echo INSERT INTO `1_vpn_req_log_cabinet` SET `id`=0, `datetime`=NOW(), `ip`='$_POST[ip]', `func`='$_POST[func]', `query`='".print_r($_POST, true)."'<br>";
+    echo "INSERT INTO `1_vpn_req_log_cabinet` SET `id`=0, `datetime`=NOW(), `ip`='$_POST[ip]', `func`='$_POST[func]', `query`='".print_r($_POST, true)."'<br>";
     $connect -> query("INSERT INTO `1_vpn_req_log_cabinet` SET `id`=0, `datetime`=NOW(), `ip`='$_POST[ip]', `func`='$_POST[func]', `query`='".print_r($_POST, true)."'");
     $log_id = $connect->insertId();
 
