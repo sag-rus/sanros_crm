bc21682d190d0095c794af22c3941c1b71bc593f
Rustem S <rustem.s@wishlistgames.com>
2024-05-14 16:09:10 +0300

    dbg

--- core/sync/API/object.php
+++ core/sync/API/object.php
@@ -641,7 +641,7 @@ function save_history_object($text){
 function save_object_request($connect, $data){
 	$data = $data['data'];
 	$connect->query("INSERT INTO object_request SET `id`=0, `time`=NOW(), `urobject`=?s, `object`=?s, `address`=?s, `uraddress`=?s, `inn`=?s, `kpp`=?s, `telephone`=?s, `fio`=?s, `email`=?s", $data['urobject'], $data['object'], $data['address'], $data['uraddress'], $data['inn'], $data['kpp'], $data['telephone'], $data['fio'], $data['email']);
-	return array('data'=>$data);
+	return array('ok'=>1);
 }
 
 
