bb71497fef8fdfb480f15ec1346623d79bf99926
Rustem S <rustem.s@wishlistgames.com>
2024-06-25 17:37:47 +0300

    dbg

--- core/object.php
+++ core/object.php
@@ -1702,6 +1702,7 @@ function edit_room($connect){
 	  'id' => $id,
       'type' => 'room'
     ];
+	return load_bounds($connect,$entity,'image');
 	ob_start();
 ?>
 <tr class="edit-room"><td colspan="7">
