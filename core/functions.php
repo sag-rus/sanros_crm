8a9a41fbbbbeb263ca1c502ef5a4388c3a9d6f5a
Rustem S <rustem.s@wishlistgames.com>
2024-07-09 18:59:09 +0300

    dbg

--- core/functions.php
+++ core/functions.php
@@ -1195,10 +1195,13 @@ function get_reward_schet($connect, $id, $type = "", $fact = false, $consider_bo
   $bank_com = 0;
   if ($exclude_bank_commission==1) {
 	$payment_status_string = " AND `payment`.`status` != 0";
-	if($only_payment_state)
+	if($only_payment_state) {
+		echo 'here1';
 		$data = $connect->getAll("SELECT sum, bank_com, type FROM payment WHERE ".$add_cond."pay_method=5 AND schet=?i".$payment_status_string, $id);
-	else
+	} else {
+		echo 'here2';
 		$data = $connect->getAll("SELECT sum, bank_com, type FROM payment WHERE pay_method in (5,6,7) AND schet=?i".$payment_status_string, $id);
+	}
 
 
 	foreach($data as $row){
