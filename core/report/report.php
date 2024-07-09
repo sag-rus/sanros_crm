f3d6375d6332984b9f3dbda3f83136d2ef760f9a
Rustem S <rustem.s@wishlistgames.com>
2024-07-09 18:46:39 +0300

    dbg

--- core/report/report.php
+++ core/report/report.php
@@ -561,14 +561,26 @@ function filter_payment($connect){
 
 			//блок расчета прибыли по платежу - начало
 			//if ($row['reckoning_exclude_bank_commission']!=1) $row['bank_com'] = 0;
-            $pay_reward = round(
+            /*$pay_reward = round(
 				(
 					(
 						$row['sum']*($row['position_reward']?$row['position_reward']:$row['object_reward'])/100
 					)
 				) - $row['sum'] * ($row['bank_com']/100) - ($row['sum']*$row['agency_commission']/100)
 				,2
+			);*/
+
+			$sum_bank_kom = $row['sum'] * ($row['bank_com']/100);
+            $pay_reward = round(
+				(
+					(
+						($row['sum']-$sum_bank_kom)*($row['position_reward']?$row['position_reward']:$row['object_reward'])/100
+					)
+				)  - ($row['sum']*$row['agency_commission']/100)
+				,2
 			);
+
+			echo 'id='.$id.' $pay_reward='.$pay_reward.'';
 
 			if($type_pay_tbl === '1-3' && $type_pay == 5) {
 			    $pay_reward *= (-1);
