f61f6c645fa028ac6498a9eb15e00b22a1cfb01b
Rustem S <rustem.s@wishlistgames.com>
2024-07-10 15:35:27 +0300

    dbg

--- core/sync/API/object.php
+++ core/sync/API/object.php
@@ -72,12 +72,15 @@ function update_contact_object_account($connect, $data){
 			'type' => 'content'
 		];
 
-		
+		try {
 		$boundsArraySliderPhotos = [];
 		$boundsArraySliderPhotos = files_to_bounds($connect,$entity,'slider_photos',isset($data['slider_photos'])?$data['slider_photos']:[]);		
 		/*remove_bounds($connect,$entity,'slider_photos');		
 		set_bounds($connect,$boundsArraySliderPhotos,'slider_photos');
 		*/
+		} catch (Exception $e) {
+			save_history_object("Ошибка: ".$e->getMessage());
+		}
 		
 
 		sync_site_content($connect, $sites_contents['id']);
