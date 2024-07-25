69e848a46d9a7be7a563ba5834b89b4a46b45d93
Rustem S <rustem.s@wishlistgames.com>
2024-07-25 17:30:09 +0500

    dbg

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "17-07-2024";
+	$version = "25-07-2024";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
