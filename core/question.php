c52fdcc8ca2e218b542e8939dae034bb059773a9
Rustem S <rustem.s@wishlistgames.com>
2024-07-31 13:13:33 +0300

    dbg

--- core/question.php
+++ core/question.php
@@ -115,7 +115,7 @@ function show_question_object($connect){
 		<div class="panel-heading"><i class="fa fa-comments-o"></i> Вопросы из Личного кабинета объекта</div>
 		<div class="list-group">
 <?php
-	$data = $connect->getAll("SELECT talk.id, talk.client, talk.category, talk.id_reck, DATE_FORMAT(max(message_talk.date), '%H:%i:%s %d.%m') as time FROM talk LEFT JOIN message_talk ON message_talk.talk=talk.id WHERE talk.type='object' GROUP BY talk.id ORDER BY message_talk.active, max(message_talk.date) DESC LIMIT 30");
+	$data = $connect->getAll("SELECT talk.id, talk.client, talk.category, talk.id_reck, DATE_FORMAT(max(message_talk.date), '%H:%i:%s %d.%m.%Y') as time FROM talk LEFT JOIN message_talk ON message_talk.talk=talk.id WHERE talk.type='object' GROUP BY talk.id ORDER BY message_talk.active, max(message_talk.date) DESC LIMIT 30");
 	foreach($data as $row){
 		$talk = $row["id"];
 		$id_reck = $row["id_reck"];
