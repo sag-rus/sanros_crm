c4abcc51128b7d050fdda25f04b4143b373edaad
Rustem S <rustem.s@wishlistgames.com>
2024-08-05 17:50:27 +0300

    dbg

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -1139,7 +1139,7 @@ function update_group($connect){
 }
 
 function show_request_object($connect){
-	$data = $connect->getAll("SELECT id, time, object, telephone, email, address, website, website_object, comment, source, status FROM object_request WHERE status>=0 ORDER BY status ASC, time DESC");
+	$data = $connect->getAll("SELECT id, DATE_FORMAT(max(message_talk.date), '%d.%m.%Y %H:%i:%s'), object, telephone, email, address, website, website_object, comment, source, status FROM object_request WHERE status>=0 ORDER BY status ASC, time DESC");
 	return json_encode($data);
 }
 
