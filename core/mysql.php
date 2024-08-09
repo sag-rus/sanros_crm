6d5eae80e7c4c8d7015d894d2eff392e6807e706
Rustem S <rustem.s@wishlistgames.com>
2024-08-09 14:43:48 +0300

    dbg

--- core/mysql.php
+++ core/mysql.php
@@ -109,7 +109,7 @@ function help_search_by_name($connect){
 	$table = $_POST["table"];
 	$func = $_POST["function"];
 	if($table == "object")
-		$data = $connect->getAll("SELECT id, name FROM object WHERE name LIKE ?s AND (id != 61 AND id != 62 AND id != 63 AND id != 64 AND id != 71)", "%".$poisk."%");
+		$data = $connect->getAll("SELECT id, name, id_object FROM object WHERE name LIKE ?s AND (id != 61 AND id != 62 AND id != 63 AND id != 64 AND id != 71)", "%".$poisk."%");
 	elseif($table == "agency")
 		$data = $connect->getAll("SELECT id, short_name, name, active FROM agency WHERE name LIKE ?s OR name LIKE ?s OR short_name LIKE ?s OR short_name LIKE ?s OR name LIKE ?s OR name LIKE ?s OR short_name LIKE ?s OR short_name LIKE ?s", "%".$poisk."%", "%".$poisk."%", "%".$poisk."%", "%".$poisk."%", "%".$poisk_quotes."%", "%".$poisk_quotes."%", $poisk_quotes."%", "%".$poisk_quotes."%");
 	elseif($table == "tour_operator")
@@ -124,9 +124,10 @@ function help_search_by_name($connect){
 			$id = $row["id"];
 			$name = $row["name"];
 			$object = get_object($connect, $id, "place");
+			if ($func=='select_object_on_request' && $row['id_object']>0) $id = -1;
 		?>
 			<span onclick="<?php echo $func; ?>(<?php echo $id; ?>, <?php if ($func!='select_object_on_request' ) echo $id; else echo '\''.$name.'\''; ?>)">
-				<?php echo $object; ?>
+				<?php echo $object; if ($func=='select_object_on_request' && $row['id_object']>0) echo '(уже привязан к аккаунту)' ?>
 			</span>
 		<?php
 		}elseif($table == "agency"){
