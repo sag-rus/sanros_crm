e50339defef87dd20ae5bb5966d593384596eb6f
Rustem S <rustem.s@wishlistgames.com>
2024-09-12 17:11:44 +0300

    dbg

--- core/lib/mail.php
+++ core/lib/mail.php
@@ -147,8 +147,8 @@ function send_mail_sanata($email, $title, $mess){
 	$conf = new JConfig;
 	//$email_from = $conf->email_from_sanata;
 	//$password_from = $conf->password_from_sanata;
-	$mail->Username = $email_from;
-	$mail->Password = $password_from;	
+	$email_from = $conf->email_from;
+	$password_from = $conf->password_from;
 	include_once($directory."/core/lib/PHPMailer/class.phpmailer.php");
 	$mail = new PHPMailer();
 	$mail->IsSMTP();
