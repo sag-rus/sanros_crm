30cdd7cebe46dd907a4c715ab858ff480f11d48a
Rustem S <rustem.s@wishlistgames.com>
2024-10-27 19:07:15 +0300

    dbg

--- core/sync/API/travelline.php
+++ core/sync/API/travelline.php
@@ -329,6 +329,7 @@ function confirm_bookings($connect, $data){
 	];
 	foreach($confirm as $book){
 		$booking = $connect->getRow("SELECT bid FROM booking WHERE id=?i", $book['number']);
+		$result['query'] = $connect->last_query();
 		if (isset($booking['id'])) {
 			$connect->query("UPDATE booking SET confirm=1, update_bid=0, id_travelline=?s WHERE id=?i", $book['externalNumber'], $book['number']);
 			//$sql = $connect->last_query();
