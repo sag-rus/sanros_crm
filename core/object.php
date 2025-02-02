da9c4d738e3648c13ec317e0ab2cbccabc95d3e9
Rustem S <rustem.s@wishlistgames.com>
2025-02-02 13:01:52 +0300

    dbg

--- core/object.php
+++ core/object.php
@@ -1746,8 +1746,9 @@ function save_new_room($connect){
         $housing = NULL;
     else
         $housing = (int)$_POST['housing'];
-	$connect->query("INSERT INTO room(name, id_obj, id_comfort, id_best_comfort, note, main_place, add_place, housing, square, food) VALUES (?s, ?i, ?s, ?s, ?s, ?i, ?i, ?s, ?s, ?s)", $_POST["name_room"], $_POST["id_obj"], $_POST["comfort"], $_POST["best_comfort"], $_POST["note"], $_POST["main_place"], $_POST["add_place"], $housing, $_POST["square"], $_POST["food"]);
-	sync_objects_api($connect);
+	$connect->query("INSERT INTO room(name, id_obj, id_comfort, id_best_comfort, note, main_place, add_place, wo_bed_place, housing, square, food) VALUES (?s, ?i, ?s, ?s, ?s, ?i, ?i, ?s, ?s, ?s)", $_POST["name_room"], $_POST["id_obj"], $_POST["comfort"], $_POST["best_comfort"], $_POST["note"], $_POST["main_place"], $_POST["add_place"], $_POST["wo_bed_place"], $housing, $_POST["square"], $_POST["food"]);
+	echo $connect->last_query();
+	//sync_objects_api($connect);
 }
 
 function edit_room($connect){
