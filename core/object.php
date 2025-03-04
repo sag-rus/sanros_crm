f500339c99b2504b2bbbdb53a1f94f5083a2d9d4
Rustem S <rustem.s@wishlistgames.com>
2025-03-04 19:34:41 +0300

    dbg

--- core/object.php
+++ core/object.php
@@ -1227,7 +1227,7 @@ function room_occupancy($connect){
 								$rooms = $connect->getAll("SELECT * FROM room WHERE active=0 AND id_obj=?i ORDER BY name ASC", $_POST['id_obj']);
 								foreach ($rooms as $room) {
 									if ($room['housing']>0) {
-										$housing = $connect->getOne("SELECT name FROM room WHERE id=?i", $room['housing']);
+										$housing = $connect->getOne("SELECT name FROM housing WHERE id=?i", $room['housing']);
 										if ($housing) $room['name'] .= '('.$housing.')';
 									}
 									if ($id_room==$room['id']) $sel = ' selected="selected"'; else $sel = '';
