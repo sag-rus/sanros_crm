b116f2e955ceb25edf11827384ed8925ce4d8889
Rustem S <rustem.s@wishlistgames.com>
2025-03-24 14:40:31 +0300

    dbg

---
+++ cktest.php
@@ -0,0 +1,100 @@
+<?php
+	$version = "05-03-2025-2";
+    $loader = require( __DIR__ . '/vendor/autoload.php');
+    /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
+
+    ]);
+    die();*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>CRM БОНУСЫ</title>
+
+	<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+    <script type="text/javascript" src="js/ckeditor/ckeditor.js?v=<?=$version;?>"></script>
+    <script type="text/javascript" src="js/ckeditor/ru.js?v=<?=$version;?>"></script>
+    <script type="text/javascript" src="js/highcharts.js"></script>
+	<script type="text/javascript" src="js/ajaxupload.js"></script>
+	<script type="text/javascript" src="js/jquery-ui.js"></script>
+	<script type="text/javascript" src="js/jquery.tablesorter.js"></script>
+	<script type="text/javascript" src="js/jquery.metadata.js"></script>
+	<script type="text/javascript" src="js/fullcalendar.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery.inputmask.js"></script>
+	<script type="text/javascript" src="js/jquery.inputmask.date.extensions.js"></script>
+	<script type="text/javascript" src="js/jquery.ba-throttle-debounce.min.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.scrollTo-min.js"></script>
+	<script type="text/javascript" src="js/strophe/strophe.js"></script>
+	<script type="text/javascript" src="js/strophe/plugin.roster.js"></script>
+	<script type="text/javascript" src="111modules/function.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-image.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-schet.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-users.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-object.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-price.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-admin.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-chat.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-head.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-agency.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-report.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-touroperator.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-profit.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-mail.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-reminder.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-promotions.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-question.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-profile.js?ver=<?php echo $version; ?>"></script>
+	<script type="text/javascript" src="modules/module-manual.js?ver=<?php echo $version; ?>"></script>
+    <script type="text/javascript" src="https://cdn.tonia.ru/static-assets/damirez-uploader/js/damirez-uploader.js?ver=<?php echo $version; ?>"></script>
+    <!--<script type="text/javascript" src="/CRM/damirez-uploader.js?ver=<?php echo $version; ?>"></script>-->
+    <!--link rel="stylesheet" href="js/ckeditor/style.css"-->
+	<link href="font/font-awesome/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css?ver=<?php echo $version; ?>">
+	<link rel="stylesheet" href="css/jquery-ui.css?ver=<?php echo $version; ?>">
+	<link rel="stylesheet" href="css/style-calendar.css?ver=<?php echo $version; ?>">
+	<link rel="stylesheet" href="css/style-color.css?ver=<?php echo $version; ?>">
+	<link rel="stylesheet" href="css/style-chat.css?ver=<?php echo $version; ?>">
+	<link rel="stylesheet" href="css/dots.css">
+	<link href="css/fullcalendar.css" rel="stylesheet" />
+	<link href="css/bootstrap.css?ver=<?php echo $version; ?>" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.tonia.ru/static-assets/damirez-uploader/css/damirez-uploader.css?ver=<?php echo $version; ?>">
+    <!--<link rel="stylesheet" href="/CRM/css/damirez-uploader.css?ver=<?php echo $version; ?>">-->
+	<link rel="shortcut icon" href="favicon.ico">
+    <meta charset="utf-8" />
+
+</head>
+<body>
+</body>
+
+<?php if ($_SERVER['REMOTE_ADDR']=='10.10.11.5') { ?>
+TEST:
+<div id="sites_content_body_test"></div>>
+<script>
+	$(document).ready(function (e) {
+		DecoupledEditor
+			.create( $('#sites_content_body_test').get(0), {
+				language: 'ru'
+			})
+			.then( editor4 => {
+
+				$('#sites_content_body_test').before('<div id="sites_content_body_test_toolbar_container"></div>');
+
+				const toolbarContainer_cabinet = $('#sites_content_body_test_toolbar_container').get(0);
+
+				toolbarContainer_cabinet.appendChild( editor4.ui.view.toolbar.element );
+
+				window.sites_content_body_test = editor4;
+				window.sites_content_body_test.setData('123');
+			})
+			.catch( error => {
+				console.error( error );
+			});		
+		});
+</script>
+<?php } ?>
+</html>
