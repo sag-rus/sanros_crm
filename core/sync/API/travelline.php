306e73f8fd8162692bbb62bd9cccf5239ab02413
rsagdiev <rsagdiev@hostkey.com>
2025-04-04 17:08:21 +0300

    dbg

--- core/sync/API/travelline.php
+++ core/sync/API/travelline.php
@@ -2,6 +2,7 @@
 
 function save_web_hook_data($connect, $data){
 	$connect->query("INSERT INTO 1_tl_webhook SET id=0, request=?s", print_r($data, true));
+	return $connect->last_query();
 }
 
 function update_room_places_quota($connect, $data){
