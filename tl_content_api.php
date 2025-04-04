adaeafe0322ebc8c7cb1a78d34c9e8923a64bdc7
rsagdiev <rsagdiev@hostkey.com>
2025-04-04 17:24:21 +0300

    dbg

---
+++ tl_content_api.php
@@ -0,0 +1,76 @@
+<?php
+use GuzzleHttp\Client;
+
+//header('Content-Type: application/json;charset=utf-8');
+//header('Access-Control-Allow-Origin: *');
+
+require_once __DIR__."/vendor/autoload.php";
+$directory = dirname(__FILE__);
+define("_FOLDERSITE_", $directory);
+
+include_once($directory."/config.php");
+$conf = new JConfig;
+$sync = $conf->sync_base;
+$unisender_api_key = $conf->unisender_api_key;
+include_once($directory."/core/functions.php");
+include_once($directory."/core/lib/Mysql.Class.php");
+
+include_once($directory."/core/lib/mail.php");
+include_once($directory."/core/lib/sms.php");
+include_once($directory."/core/lib/PHPMailer/class.phpmailer.php");
+
+$clientCabinet = array(
+	"link" => $conf->turist_cabinet
+);
+$objectCabinet = array(
+	"link" => $conf->object_cabinet
+);
+$mail = array(
+	"module" => $conf->email_module
+);
+
+$connect = connect_to_MySQL_directory();
+$config = ConfigCRM::getInstance();
+$config->connect = $connect;
+$config->directory = $directory;
+$config->clientCabinet = $clientCabinet;
+$config->objectCabinet = $objectCabinet;
+
+$configNew = \App\lib\CRM\Config\Client::getInstance();
+$configNew->connect = $connect;
+$configNew->directory = $directory;
+$configNew->clientCabinet = $clientCabinet;
+$configNew->objectCabinet = $objectCabinet;
+
+
+
+/* ---------------------------- */
+$response = array();
+$id_obj = 922;
+$account_id = 34311;
+
+
+//AUTH
+$url = 'https://partner.tlintegration.com/auth/token';
+$data = array(
+    "grant_type"=> 'client_credentials', 
+    "client_id" => 'chm_sr2',
+    "password" => 'T5AipL1NMo61LdA3xwnKb7cGbldbaDpS'
+);
+$postdata = json_encode($data);
+$ch = curl_init($url); 
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+$result = json_decode(curl_exec($ch), true);
+curl_close($ch);
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+//AUTH
+
+?>
