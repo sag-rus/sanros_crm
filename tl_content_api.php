f5a6de287db2be186b8c97cbbf5b9e496feb8db9
rsagdiev <rsagdiev@hostkey.com>
2025-04-04 17:35:35 +0300

    dbg

--- tl_content_api.php
+++ tl_content_api.php
@@ -78,7 +78,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo '<pre>$result';
+echo '<pre>';
 print_r(json_decode($result, true));
 echo '</pre>';
 
