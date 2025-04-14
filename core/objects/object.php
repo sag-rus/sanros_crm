3a1447fbaf0a615cadcd6fe60601e5041f7be8f1
rsagdiev <rsagdiev@hostkey.com>
2025-04-14 21:30:42 +0300

    dbg

--- core/objects/object.php
+++ core/objects/object.php
@@ -1331,6 +1331,7 @@ function tl_webhook_work($connect) {
 				$occu['adult_on_main_place'] = $place['count'];
 				$export_id = get_place_export_id($room_id, $occu);
 				$connect->query("INSERT INTO `place` SET `id`=0, `status`=1, `name`='".$place['count']." взр. на осн.месте', `export_id`=?s, `id_obj`=?i, `id_room`=?i, `type`=1, `adult_on_main_place`=?i", $export_id, $data['id_obj'], $room_id, $place['count']);
+				echo $connect->last_query().'<br>';
 			}
 		}
 
