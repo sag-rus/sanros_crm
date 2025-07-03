cdfbfb8a06e04347643d0313bc5bd52c6d8f0cc7
rsagdiev <rsagdiev@hostkey.com>
2025-07-03 22:52:28 +0300

    dbg

--- core/object.php
+++ core/object.php
@@ -1919,7 +1919,7 @@ function deletetldata($connect) {
 	echo "UPDATE object SET check_places=0 WHERE id=$id<br>";
 	echo "UPDATE room SET accessible_places='', price_places='' WHERE id_obj=$id";
 	$connect->query("UPDATE object SET check_places=1 WHERE id=$id");
-	$connect->query("UPDATE room SET accessible_places='', accessible_places='' WHERE id_obj=$id");
+	$connect->query("UPDATE room SET accessible_places='', price_places='' WHERE id_obj=$id");
 }
 
 function object_check_archive($connect){
