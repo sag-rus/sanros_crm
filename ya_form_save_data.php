53df6262fb4fce5d7f4a37b812c83cfada152bd2
rsagdiev <rsagdiev@hostkey.com>
2025-08-04 18:20:24 +0300

    dbg

--- ya_form_save_data.php
+++ ya_form_save_data.php
@@ -77,13 +77,13 @@ $client_info = array(
 	"ip" => ""
 );
 
-/*
+
 $user_id = $create_client->create_client($client_info);
 
 $connect->query("INSERT INTO `reckoning` (`id`, `type`, `date`, `sum`, `count_payment`, `count_holding`, `manager`, `turist`, `id_user`, `active`, `status`, `holding`, `holding_sum`, `holding_cancelled_sum`, `holding_confirmed_sum`, `rest`, `prepay`, `id_obj`, `date_z`, `status_san`, `number_turist`, `payer`, `agency`, `id_com`, `note`, `id_dis`, `id_services`, `id_tour`, `status_agent`, `schet_san`, `date_schet_san`, `date_v`, `hash`, `website`, `website_from`, `reason_delete`, `changes`, `guaranteed`, `doc_schet_san`, `reward`, `note_bid`, `correction`, `commission_value`, `source`, `promo_code`, `form_booking`, `exclude_bank_commission`, `bank_com_auto_excluded`, `state_program`, `is_test`, `children_rest`, `far_east`, `bnovo`, `bnovo_json`, `afl`, `afl_worked`) 
                                   VALUES ('0', '0', '".date('Y-m-d')."', NULL, '0', '0', NULL, '$user_id', NULL, '0', '1', '0', '0.00', '0.00', '0.00', NULL, '0.00', '0', '".date('Y-m-d', time() + 86400)."', '0', '1', NULL, NULL, NULL, ?s, NULL, NULL, NULL, '0', NULL, NULL, '".date('Y-m-d', time() + 86400*2)."', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1', NULL, NULL, '0', '0', '0', '0', '0', '0', '0', '\'\'', '', '0');", $cmnt);
 
-*/
+
 
 $response['ok'] = 1;
 $response['sql'] = $connect->last_query();
