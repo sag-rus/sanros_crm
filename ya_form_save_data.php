8b1475804826d7b956e537f6ffa41deaab76558c
rsagdiev <rsagdiev@hostkey.com>
2025-08-04 12:59:14 +0300

    dbg

--- ya_form_save_data.php
+++ ya_form_save_data.php
@@ -53,6 +53,22 @@ $configNew->objectCabinet = $objectCabinet;
 
 $connect->query("INSERT 1_ya_form_log SET `id`=0, `datetime`=NOW(), `data`=?s", $_POST['data']);
 
+$data = json_decode($_POST['data'], true);
+$cmnt = 'Регион: '.$data['answer']['data']['answer_short_text_59666938']['value'].'<br>';
+$cmnt .= 'Цель: '.$data['answer']['data']['answer_choices_59659035']['value'][0]['text'].'<br>';
+$cmnt .= 'Профиль: '.$data['answer']['data']['answer_choices_59659370']['value'][0]['text'].'<br>';
+$cmnt .= 'Цена: '.$data['answer']['data']['answer_choices_59658680']['value'][0]['text'].'<br>';
+$cmnt .= 'Дата: '.$data['answer']['data']['answer_choices_59666247']['value'][0]['text'].'<br>';
+$cmnt .= 'Водоём: '.$data['answer']['data']['answer_choices_59666734']['value'][0]['text'].'<br>';
+$cmnt .= 'Имя отчество: '.$data['answer']['data']['answer_short_text_59643772']['value'].'<br>';
+$cmnt .= 'Телефон: '.$data['answer']['data']['answer_phone_59643876	']['value'].'<br>';
+$cmnt .= 'E-mail: '.$data['answer']['data']['answer_non_profile_email_59643896']['value'].'<br>';
+$cmnt .= 'Способ связи: '.$data['answer']['data']['answer_choices_59666785']['value'][0]['text'].'<br>';
+$cmnt .= 'Комментарий: '.$data['answer']['data']['answer_long_text_59644358']['value'].'<br>';
+
+$connect->query("INSERT INTO `reckoning` (`id`, `type`, `date`, `sum`, `count_payment`, `count_holding`, `manager`, `turist`, `id_user`, `active`, `status`, `holding`, `holding_sum`, `holding_cancelled_sum`, `holding_confirmed_sum`, `rest`, `prepay`, `id_obj`, `date_z`, `status_san`, `number_turist`, `payer`, `agency`, `id_com`, `note`, `id_dis`, `id_services`, `id_tour`, `status_agent`, `schet_san`, `date_schet_san`, `date_v`, `hash`, `website`, `website_from`, `reason_delete`, `changes`, `guaranteed`, `doc_schet_san`, `reward`, `note_bid`, `correction`, `commission_value`, `source`, `promo_code`, `form_booking`, `exclude_bank_commission`, `bank_com_auto_excluded`, `state_program`, `is_test`, `children_rest`, `far_east`, `bnovo`, `bnovo_json`, `afl`, `afl_worked`) 
+                                  VALUES ('0', '0', '".date('Y-m-d')."', NULL, '0', '0', NULL, '177039', NULL, '0', '1', '0', '0.00', '0.00', '0.00', NULL, '0.00', '0', '2025-09-01', '0', '1', NULL, NULL, NULL, ?s, NULL, NULL, NULL, '0', NULL, NULL, '2025-09-30', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1', NULL, NULL, '0', '0', '0', '0', '0', '0', '0', '\'\'', '', '0');", $cmnt);
+
 $response['ok'] = 1;
 $response['sql'] = $connect->last_query();
 
