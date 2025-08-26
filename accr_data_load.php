b5f38a38b8e324c4dbaf6977d37729470dfe364d
rsagdiev <rsagdiev@hostkey.com>
2025-08-26 12:39:39 +0300

    dbg

---
+++ accr_data_load.php
@@ -0,0 +1,52 @@
+<?php
+use GuzzleHttp\Client;
+
+//header('Content-Type: application/json;charset=utf-8');
+//header('Access-Control-Allow-Origin: *');
+
+require_once __DIR__."/vendor/autoload.php";
+$directory = dirname(__FILE__);
+define("_FOLDERSITE_", $directory);
+
+include_once($directory."/config.php");
+$conf = new JConfig;
+$sync = $conf->sync_base;
+$unisender_api_key = $conf->unisender_api_key;
+include_once($directory."/core/functions.php");
+include_once($directory."/core/lib/Mysql.Class.php");
+
+include_once($directory."/core/lib/mail.php");
+include_once($directory."/core/lib/sms.php");
+include_once($directory."/core/lib/PHPMailer/class.phpmailer.php");
+
+$clientCabinet = array(
+	"link" => $conf->turist_cabinet
+);
+$objectCabinet = array(
+	"link" => $conf->object_cabinet
+);
+$mail = array(
+	"module" => $conf->email_module
+);
+
+$connect = connect_to_MySQL_directory();
+$config = ConfigCRM::getInstance();
+$config->connect = $connect;
+$config->directory = $directory;
+$config->clientCabinet = $clientCabinet;
+$config->objectCabinet = $objectCabinet;
+
+$configNew = \App\lib\CRM\Config\Client::getInstance();
+$configNew->connect = $connect;
+$configNew->directory = $directory;
+$configNew->clientCabinet = $clientCabinet;
+$configNew->objectCabinet = $objectCabinet;
+
+
+$items = $connect->getAll("SELECT * FROM `accr_data` WHERE `data`='' OR `data_datetime` < NOW() - INTERVAL 3 MONTH ORDER BY id LIMIT 5");
+
+print_r($items);
+
+//echo '<meta http-equiv="refresh" content="0,URL=/CRM/accr_data_load.php">';
+
+?>
