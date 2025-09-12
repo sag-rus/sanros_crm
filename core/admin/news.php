6322a4d4c4478d75fe71316ba8974d07e646c13a
rsagdiev <rsagdiev@hostkey.com>
2025-09-12 19:19:34 +0300

    dbg

--- core/admin/news.php
+++ core/admin/news.php
@@ -3954,10 +3954,10 @@ function sync_bounds($connect,$entity) {
     $data["token"] = '7db0d2680968f87e33dd3db9a4b5db38d373ba8a9f42ca7dc97d6f14711efaa4';
     $data["bounds"] = $bounds;
 
-    //echo "Отправка запроса на https://sites.tonia.ru/api/".$entity['type']."/".$entity['id']."/bounds/set<br>";
-    //echo '<pre>data';
-    //print_r($data);
-    //echo '</pre>';
+    echo "Отправка запроса на https://sites.tonia.ru/api/".$entity['type']."/".$entity['id']."/bounds/set<br>";
+    echo '<pre>data';
+    print_r($data);
+    echo '</pre>';
 
     $res = $client->request('POST',"https://sites.tonia.ru/api/".$entity['type']."/".$entity['id']."/bounds/set",[
       'form_params' => $data
@@ -3965,9 +3965,9 @@ function sync_bounds($connect,$entity) {
     
     $res = json_decode($res->getBody(),true);
 
-    //echo '<pre>res';
-    //print_r($res);
-    //echo '</pre>';
+    echo '<pre>res';
+    print_r($res);
+    echo '</pre>';
 
     if(array_key_exists('success',$res)) {
       return (bool)(int)$res['success'];
