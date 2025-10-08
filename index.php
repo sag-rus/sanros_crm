d5fd4539949b8315a6fdc8dc94c29079c780fb8d
rsagdiev <rsagdiev@hostkey.com>
2025-10-08 11:20:03 +0300

    dbg

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "12-09-2025-2";
+	$version = "08-109-2025";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
--- modules/module-price.js
+++ modules/module-price.js
@@ -661,6 +661,7 @@ function update_service(id){
 function months(id_location = -1){
 	select_menu('months_open', '2');
 	if (id_location==-1) {
+		alert('000');
 		if ($('.filter_location:visible').length>0) {
 			alert('123');
 			id_location = $('.filter_location:visible option:selected').val();
