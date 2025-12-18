12a8249730ff26c822e276a968083e7c8263aa36
rsagdiev <rsagdiev@hostkey.com>
2025-12-18 10:46:55 +0300

    dbg

--- core/report/report.php
+++ core/report/report.php
@@ -1210,7 +1210,7 @@ function calendar_report($connect){
 			$month = 1;
 			$year_total = 0;
 			$year++;
-			$html.= "<strong>Итого: ".$count."</strong><br />";
+			$html.= "<strong>Итого: ".$year_total."</strong><br />";
 			if($current_year >= $year)
 				$html.= "</div><div style='float: left; margin-left: 10px;'>".$year." год<br />";
 		}
