1a4311f0ad72fc9fd30829ffdebccd4838f748be
rsagdiev <rsagdiev@hostkey.com>
2025-12-23 16:39:38 +0300

    dbg

--- core/document/schet.php
+++ core/document/schet.php
@@ -375,17 +375,24 @@ function review_schet($connect, $type = "PDF", $id, $for = ""){
 			echo "<tr><td colspan='3' style='border: none;'>&nbsp;<br /></td></tr>";
 	?>
 	</table>
-	<?php if(!$agency) {
+	<?php 
+	if(!$agency) {
 		echo "<span class='bold_head'>Оплатив счет, Вы получите 2% бонус на следующий заказ</span>";
 		$configNew->account = $turist;
 		$configNew->booking = $id;
 
-    $payment = new \App\lib\payment\Alfa\BookingPayment([]);
-    $qr_data = $payment->registerPayment('pay', '1');
+    	$payment = new \App\lib\payment\Alfa\BookingPayment([]);
+    	$qr_data = $payment->registerPayment('pay', '1');
 
-    if ($qr_data['renderedQr']!='') echo '<br><br><span class="bold_head" style="color: red;">Для оплаты через СБП отсканируйте QR-код камерой телефона:</span><br><br><img src="data:image/png;base64, '.$qr_data['renderedQr'].'"/>';
+		if ($id==182561) {
+			echo '<pre>';
+			print_r($qr_data);
+			echo '</pre>';
+		}
 
-    if ($qr_data['payload']!='') echo '<br><br><span class="bold_head">или нажмите на кнопку:</span><br><br><a href="'.$qr_data['payload'].'" class="btn"><img src="images/pay_btn.jpg" /><br /></a>';
+    	if ($qr_data['renderedQr']!='') echo '<br><br><span class="bold_head" style="color: red;">Для оплаты через СБП отсканируйте QR-код камерой телефона:</span><br><br><img src="data:image/png;base64, '.$qr_data['renderedQr'].'"/>';
+
+    	if ($qr_data['payload']!='') echo '<br><br><span class="bold_head">или нажмите на кнопку:</span><br><br><a href="'.$qr_data['payload'].'" class="btn"><img src="images/pay_btn.jpg" /><br /></a>';
 	} ?>
 	</div>
 
