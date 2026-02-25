897d3684970d43191142009405267cbf82f97916
rsagdiev <rsagdiev@hostkey.com>
2026-02-25 14:54:43 +0300

    dbg

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$version = "12-02-2026-2";
+	$version = "25-02-2026";
     $loader = require( __DIR__ . '/vendor/autoload.php');
     /*$payment = new \App\lib\payment\Sberbank\BookingPayment([
 
